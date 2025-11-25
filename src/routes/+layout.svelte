c7ea7e1fc6d2eb747895cac0dfd09bec8ad3070d
Lewin Kelly <heli@odex.cf>
2025-11-25 00:16:18 +0000

    Noise implementation, map with grass and mountains depending on height

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
+	import { applyAction } from "$app/forms"
 	import favicon from "$lib/assets/favicon.svg"
+	import "/src/app.css"
 
 	let { children } = $props()
 </script>
 
 <svelte:head>
+	<title>BoredGame</title>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<main>
+	{@render children()}
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a>
-	 to read the documentation
-</p>
+<script lang="ts">
+	import { noise } from "$lib/noise"
+
+	const width = 21
+	const height = 21
+
+	const seed = 0.5
+
+	const scale = 15
+	const map = Array.from({ length: height }, (_, y) =>
+		Array.from({ length: width }, (_, x) =>
+			noise(x / scale, y / scale, seed)
+		)
+	)
+</script>
+
+{#each Array(height) as _, y}
+	<div class="row">
+		{#each Array(width) as _, x}
+			<div class={["cell", map[y][x] > 0.6 ? "mountain" : "grass"]}></div>
+		{/each}
+	</div>
+{/each}
+
+<style>
+	.row {
+		display: flex;
+	}
+
+	.cell {
+		width: 2rem;
+		height: 2rem;
+		box-sizing: border-box;
+		border: 2px solid black;
+	}
+
+	.mountain {
+		background-color: grey;
+	}
+
+	.grass {
+		background-color: green;
+	}
+</style>
